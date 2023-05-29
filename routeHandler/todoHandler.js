4a01d513cd169c81e640c20b4936632d5aba0032
sowmik-sec <sowmik.sec@gmail.com>
2023-05-29 11:54:22 +0600

    create object data model with mongoose

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require("../schemas/todoSchema");
+const Todo = new mongoose.model("Todo", todoSchema);
 
 // GET ALL THE TODOS
 router.get("/", async (req, res) => {});
